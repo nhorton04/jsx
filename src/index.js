e256028f3ad06e7c227b342da161873b67d98893
Nick <nhorton04@gmail.com>
2018-12-05 21:46:17 -0800

    add buttonText

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 //Import the react and react-dom libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 //Create a react component
 const App = () => {
-  return <div> My function :)</div>;
+  const buttonText = "Click Me!";
+  return (
+    <div>
+      <label className="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText}
+      </button>
+    </div>
+  );
 };
 
 //Take the react component and show it on the screen
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
